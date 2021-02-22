5d9542539e1b56eb95ebcae228e1b0453f635843
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2021-02-22 18:21:09 +0100

    Añado a LibreriaIN-OUT ,se crea o  .gitmodules e importo a libreria para chamar aos metodos

--- src/com/ejercicioscod/Main.java
+++ src/com/ejercicioscod/Main.java
@@ -1,5 +1,4 @@
 package com.ejercicioscod;
-
 public class Main {
 
     public static void main(String[] args) {
--- src/com/ejercicioscod/ejercicioLibreria/Main.java
+++ src/com/ejercicioscod/ejercicioLibreria/Main.java
@@ -1,7 +1,11 @@
 package com.ejercicioscod.ejercicioLibreria;
-
+import metodos.Metodos;
 public class Main {
     public static void main(String[] args) {
+
+        Metodos metodos= new Metodos();
+        metodos.mensaxePorConsola("Mensaxe que se imprime por consola");
+        metodos.mensaxeVentanaEscritorio("Mensaxe que se imprime nunha ventana dun escritorio");
 
     }
 }
